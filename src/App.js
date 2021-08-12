f7d9e7360c3af0b465b0612e88b4825523e6a3cc
nderimbajrami <bajraminderim@outlook.com>
2021-08-12 15:13:26 +0200

    change bootstrap version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h2>test</h2>
+    <div>
+      <Navbar/>
+      <h2>test</h2>
+    </div>
   );
 }
 
